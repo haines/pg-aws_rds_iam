94b349719e859eb560b5b44322a0e214a741f80d
Andrew Haines <andrew@haines.org.nz>
2021-03-21 21:55:24 +0000

    Rename acceptance test resources

--- test/acceptance/infrastructure/rds.tf
+++ test/acceptance/infrastructure/rds.tf
@@ -2,15 +2,15 @@ locals {
   username = "test_user"
 }
 
-resource "aws_db_instance" "postgresql_test" {
-  identifier             = "test"
+resource "aws_db_instance" "pg_aws_rds_iam_test" {
+  identifier             = "pg-aws-rds-iam-test"
   instance_class         = "db.t2.micro"
   engine                 = "postgres"
-  engine_version         = "11.5"
-  allocated_storage      = 20
+  engine_version         = "12.5"
+  allocated_storage      = 20 # GiB
   skip_final_snapshot    = true
   publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.postgresql_test.id]
+  vpc_security_group_ids = [aws_security_group.pg_aws_rds_iam_test.id]
 
   username = "root"
   password = var.password
@@ -19,21 +19,21 @@ resource "aws_db_instance" "postgresql_test" {
   iam_database_authentication_enabled = true
 }
 
-resource "null_resource" "postgresql_test_user" {
+resource "null_resource" "pg_aws_rds_iam_test_user" {
   depends_on = [
-    aws_db_instance.postgresql_test,
-    aws_security_group_rule.postgresql_test_ingress_allow_postgresql_from_current_ip,
+    aws_db_instance.pg_aws_rds_iam_test,
+    aws_security_group_rule.pg_aws_rds_iam_test_ingress_allow_postgresql_from_current_ip,
   ]
 
   provisioner "local-exec" {
     command = "psql --command='CREATE USER ${local.username}; GRANT rds_iam TO ${local.username};'"
 
     environment = {
-      PGHOST     = aws_db_instance.postgresql_test.address
-      PGPORT     = aws_db_instance.postgresql_test.port
-      PGUSER     = aws_db_instance.postgresql_test.username
-      PGPASSWORD = aws_db_instance.postgresql_test.password
-      PGDATABASE = aws_db_instance.postgresql_test.name
+      PGHOST     = aws_db_instance.pg_aws_rds_iam_test.address
+      PGPORT     = aws_db_instance.pg_aws_rds_iam_test.port
+      PGUSER     = aws_db_instance.pg_aws_rds_iam_test.username
+      PGPASSWORD = aws_db_instance.pg_aws_rds_iam_test.password
+      PGDATABASE = aws_db_instance.pg_aws_rds_iam_test.name
     }
   }
 }
